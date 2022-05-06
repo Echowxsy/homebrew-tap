b9dd5afd4c6c7e2566aa515c1180e7afa0e48c5f
Echowxsy <echowxsy@gmail.com>
2022-05-06 11:45:29 +0800

    Remove ja-netfilter depend

--- Formula/ja-netfilter.rb
+++ Formula/ja-netfilter.rb
@@ -4,7 +4,6 @@ class JaNetfilter < Formula
   url "https://echowxsy.cn/ja-netfilter-all-7d68b53deb1b1a16f8e95ecf8f3f98805d18368d.zip"
   sha256 "9016a6679a4ee65316aba342454e5e04ec7bd1c932ab36607e311a29c4a90754"
   license "GPL-3.0-or-later"
-  depends_on "openjdk"
   def install
     libexec.install Dir["*"]
   end
