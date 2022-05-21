e875b2c96ee2aae9d3e387b2072b09484cef7a5f
Echowxsy <echowxsy@gmail.com>
2022-05-21 21:32:06 +0800

    Add input-source-pro

---
+++ Casks/input-source-pro.rb
@@ -0,0 +1,10 @@
+cask 'input-source-pro' do
+  version '2.0.5'
+  sha256 '4afc880bae4bf78e713b4d302a2ab63ccb04f710c49ea0bff3089489b797334a'
+  url "https://inputsource.pro/beta/Input%20Source%20Pro%20#{version}-beta.dmg"
+  name 'Input Source Pro'
+  desc 'A tool for multi-language users '
+  desc '通过简洁的提示方式，配合输入法自动切换功能，让每一次输入都信心十足。'
+  homepage 'https://inputsource.pro'
+  app 'Input Source Pro.app'
+end
